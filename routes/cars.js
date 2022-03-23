c4a9439ccc5398abb640018ab0bba321536abbac
kumudu-dodandeniya <69890196+kumudu-dodandeniya@users.noreply.github.com>
2022-03-23 13:11:41 +0530

    create get ,put, delete method

--- routes/cars.js
+++ routes/cars.js
@@ -1,11 +1,12 @@
 const express = require ("express");
+const cars = require("../models/cars");
 const Cars = require('../models/cars')
 const router = express.Router();
 
 //save cars
 router.post('/car/save', (req,res) => {
 
-    let newCar = new Cars(req.body);
+    let newCar = new Cars(reqbody);
 
     newCar.save ((err) => {
         if(err){
@@ -19,8 +20,59 @@ router.post('/car/save', (req,res) => {
     });
 });
 
-module. exports = router;
-
-router.get("/", (req, res, next) => {
+//get cars
+router.get("/cars", (req, res) => {
+    Cars.find().exec((err,cars) =>{
+        if(err){
+            return res.status(400).json({
+                error:err
+            });
+        } 
+        return res.status(200).json({
+            success: true,
+            existingCars:cars
+        });
+    });
     
-})
+});
+
+//update cars
+router.put('/cars/update/:id', (req, res) => {
+    Cars.findByIdAndUpdate(
+        req.params.id,
+        {
+            $set:req.body
+        },
+        (err,cars) =>{
+        if(err){
+            return res.status(400).json({
+                error:err
+            });
+        } 
+        return res.status(200).json({
+            success: "Update Succesfully"
+            
+        });
+    });
+    
+});
+
+//delete cars
+router.delete("/cars/delete/:id", (req, res) => {
+    Cars.findByIdAndRemove(req.params.id).exec((err,deleteCar) =>{
+        if(err){
+            return res.status(400).json({
+                message: "Delete unsuccesful",err
+            });
+        } 
+        return res.status(200).json({
+            success: true,
+            message: "Delete succesful",deleteCar
+        });
+    });
+    
+});
+
+
+
+module. exports = router;
