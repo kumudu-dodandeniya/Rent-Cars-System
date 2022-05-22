fdd08ddd04e208276bb67d9bafc8ea97f8145fd0
Sudeepa Kahandawa Arachchi <88578025+ssisari-3789@users.noreply.github.com>
2022-05-22 06:34:35 +0530

    updated client

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'bg-homepage': "url(../src/components/assets/bg.jpg)"
+      }
+    },
+  },
+  plugins: [],
+}
